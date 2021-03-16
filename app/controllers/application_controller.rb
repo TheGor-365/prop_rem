a9ede0193499443e056e10cd7d910cd7d3a0d490
Gor <7495500@gmail.com>
2021-03-16 17:39:17 +0300

    Initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,25 @@
+class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    @show_sidebar = true if account_signed_in?
+    devise_parameter_sanitizer.permit(
+      :account_update, keys: [
+        :first_name,
+        :last_name,
+        :telephone,
+        :url,
+        :image,
+        :image_cache,
+        :cover_image,
+        :cover_image_cache,
+        :company,
+        :image_cache,
+        :details
+      ]
+    )
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+
+  devise_for :accounts
+  resources :posts
+  resources :properties
+
+  get '/blog' => 'posts#latest', as: :blog
+
+  get '/accounts' => 'admin#accounts', as: :accounts
+
+  get '/dashboard' => 'dashboard#index', as: :dashboard
+  get '/profile/:id' => 'dashboard#profile', as: :profile
+  post 'agent/message' => "properties#email_agent", as: :email_agent
+
+  root to: 'public#main'
+
+end
